3403ccaf3b299748ba67ca52a8d338e7f4d8506b
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-06-05 17:45:01 +0100

    finifhed designed

--- components/userdashboard/SidedashbaordIcon.jsx
+++ components/userdashboard/SidedashbaordIcon.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function SidedashbaordIcon({ icon, name, path, classes, class_text, close=() => {}  }) {
   return (
    <div>
-      <Link href={path}>
+      <Link href={path} as={path}>
       <div className={`flex  items-center mb-4 py-2 justify-start ${classes}`} onClick={() => close(false)}>
         <div className={`flex items-center mr-2 text-[#6B6D72] text-xl ${class_text}`}>
           <i className={icon}></i>
